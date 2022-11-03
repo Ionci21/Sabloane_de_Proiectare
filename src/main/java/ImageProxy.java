448e0b753b5bb4a95e195769c7162c61f450c292
Mercescu Ionut <ionut.mercescu00@e-uvt.ro>
2022-11-03 12:08:08 +0200

    Laborator IV

---
+++ src/main/java/ImageProxy.java
@@ -0,0 +1,37 @@
+@SuppressWarnings("unused")
+public class ImageProxy implements Element, Picture {
+    private final String proxyImage;
+    private int imageSize;
+    private Image imageObject;
+
+    public ImageProxy(String proxyImage) {
+        this.proxyImage = proxyImage;
+    }
+
+    public Image loadImage() {
+        if (imageObject == null) {
+            imageObject = new Image(proxyImage);
+        }
+        return imageObject;
+    }
+
+    @Override
+    public void print() {
+        loadImage().print();
+    }
+
+    @Override
+    public void add(Element element) {
+
+    }
+
+    @Override
+    public void remove(Element element) {
+
+    }
+
+    @Override
+    public Element get(int indexulElementului) {
+        return null;
+    }
+}
